45077fca6aa492d630aa057bf097644e23ed2b56
fedko-zhenja <fedkozhenja@gmail.com>
2022-01-25 17:25:47 +0300

    Adding task: Construction switch. Task one

---
+++ 1. Основы JavaScript/Конструкция-switch/Задание-1/script.js
@@ -0,0 +1,7 @@
+if (browser == 'Edge') {
+alert( "You've got the Edge!" );
+} else if (browser == 'Chrome' || 'Firefox' || 'Safari' || 'Opera') {
+alert( 'Okay we support these browsers too' );
+} else {
+alert( 'Okay we support these browsers too' );
+}
